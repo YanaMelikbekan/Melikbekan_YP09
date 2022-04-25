7938d709788c54a00beb88275975989d6ea4f84c
YanaMelikbekan <melikbekyan.jana@yandex.ru>
2022-04-25 10:17:03 +0300

    modalWindowThree

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import{modalWindow} from './modalWindow.js'
 import{accordion} from './accordion.js'
 import{modalWindowTwo} from './modalWindowTwo.js'
+import{modalWindowThree} from './modalWindowThree.js'
 modalWindow()
 accordion()
 modalWindowTwo()
+modalWindowThree()
---
+++ js/modalWindowThree.js
@@ -0,0 +1,13 @@
+export function modalWindowThree() {
+    const button = document.querySelector('.check-btn ');
+    const windowBlock = document.querySelector('.popup-check')
+    button.addEventListener('click', () => {
+        windowBlock.style.display = 'block'
+    })
+    windowBlock.addEventListener('click', (e) => {
+        e.preventDefault()
+        if ((e.target.classList.contains('popup-close')) || (e.target.classList.contains('popup-call'))) {
+            windowBlock.style.display = 'none'
+        }
+    })
+}
